c37b06489a7620fed8f76ec7825fab8a185fcbdb
prescience <prescience@prescience-laptop.(none)>
2012-08-01 20:01:01 +0530

    modifife

--- app/controllers/search_tweets_controller.rb
+++ app/controllers/search_tweets_controller.rb
@@ -13,13 +13,21 @@ class SearchTweetsController < ApplicationController
 
         @hashOfResponse = JSON.parse(@generated_json)
         @tweets_count = []
+	
 
             @hashOfResponse.each { |trend|
                     @tweets_count << trend['text'].split.size
             }
 
-	
+	@actual_count = Hash.new(0)
 
+	# iterate over the array, counting duplicate entries	
+	  @tweets_count.each do |v|
+  		@actual_count[v] += 1
+	end
+
+	@count = @actual_count.sort_by {|key,value| value }
+	puts " ----------- #{@count}"
 		respond_to do |format|
           	    format.html {} 
     		end  
