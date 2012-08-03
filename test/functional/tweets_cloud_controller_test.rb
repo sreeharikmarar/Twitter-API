4efb194f8d749fc79b11a00bd9cbd9380ff7fb85
prescience <prescience@prescience-laptop.(none)>
2012-08-03 19:37:18 +0530

    modifications

---
+++ test/functional/tweets_cloud_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TweetsCloudControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/tweets_cloud_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TweetsCloudHelperTest < ActionView::TestCase
+end
