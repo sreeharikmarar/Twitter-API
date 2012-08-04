87506e53d808bdbc002d1c8e5288996084067bb6
prescience <prescience@prescience-laptop.(none)>
2012-08-04 15:02:14 +0530

    modified the error messages

--- app/controllers/tweets_cloud_controller.rb
+++ app/controllers/tweets_cloud_controller.rb
@@ -1,9 +1,13 @@
 class TweetsCloudController < ApplicationController
-
+  
+    
 	def index
+
+    @response_text = params[:response_text]
     respond_to do |format|
       format.html {}
     end
+    
 	end
 
 	def cloud_tag
@@ -12,51 +16,63 @@ class TweetsCloudController < ApplicationController
     user = params["username"].to_s
 
 
-    #response =  RestClient.get("https://twitter.com/statuses/user_timeline/"+user+".json")[
-    response =  RestClient.get("http://api.twitter.com/1/statuses/user_timeline.json?id="+user+"&count=200&page=1&include_rts=true"){|response, request, result| response }
+    #    response =  RestClient.get("https://twitter.com/statuses/user_timeline/"+user+".json"){|response, request, result| response }
+    response =  RestClient.get("http://api.twitter.com/1/statuses/user_timeline.json?id="+user+"&count=200&page=1"){|response, request, result| response }
 
 
-#    puts "&"*100
-#    puts "RESPONSE CODE : #{response.code}"
-#    puts "&"*100
-#    puts "RESPONSE TO STRING : #{response.to_str}"
-#    puts "&"*100
-#    puts "RESPONSE HEADERS : #{response.headers}"
+    puts "*"*100
+    puts "retweets_count : #{response.code}"
+    puts "*"*100
+    if response.code == 200
+      @generated_json = response.body
 
 
-    @generated_json = response.body
+      @hashOfResponse = JSON.parse(@generated_json)
+
+      @tweets_count = []
+      
+      @hashOfResponse.each { |res|
+        @tweets_count << res['text'].split.size
+      }
+
+      #calculating retweets count
+      
+      @re_tweets_count = []
+      @hashOfResponse.each { |re_twt|
+        @re_tweets_count << re_twt['retweet_count']
+      }
 
 
-    @hashOfResponse = JSON.parse(@generated_json)
 
-    @tweets_count = []
-    @re_tweets_count = []
+      puts "^"*100
+      puts "retweets_count : #{@re_tweets_count}"
+      puts "^"*100
 
-    @hashOfResponse.each { |res|
-      @tweets_count << res['text'].split.size
-    }
-    
-    @hashOfResponse.each { |re_twt|
-      @re_tweets_count << re_twt['retweet_count']
-    }
+      
+      # iterate over the array, counting duplicate entries
+      @actual_count = Hash.new(0)
 
-   
-    
-    puts "^"*100
-    puts "retweets_count : #{@re_tweets_count}"
-    puts "^"*100
-    
-    @actual_count = Hash.new(0)
+      
+      @tweets_count.each do |v|
+        @actual_count[v] += 1
+      end
 
-    # iterate over the array, counting duplicate entries
-	  @tweets_count.each do |v|
-  		@actual_count[v] += 1
-    end
+      @count = @actual_count.sort_by {|key,value| value }.reverse
 
-     @count = @actual_count.sort_by {|key,value| value }.reverse
-     
-		respond_to do |format|
-      format.html {}
+      respond_to do |format|
+        format.html {}
+      end
+    elsif response.code == 404
+      @response_text  =  " Sorry That Page Doesnot Found"
+      respond_to do |format|
+        format.html {redirect_to root_path(:response_text=>@response_text)}
+      end
+
+    else
+      @response_text  =  " Not Found"
+      respond_to do |format|
+        format.html {redirect_to root_path(:response_text=>@response_text)}
+      end
     end
   
   end
