b859abc85c77920fdf105aec27442faf2de384dc
sreeharikmarar <sreeharikmarar@gmail.com>
2012-08-05 00:56:27 +0530

    changes

--- app/controllers/tweets_cloud_controller.rb
+++ app/controllers/tweets_cloud_controller.rb
@@ -2,7 +2,7 @@ class TweetsCloudController < ApplicationController
   
     
 	def index
-
+    @back_ground_url = 'assets/original2.jpg'
     @response_text = params[:response_text]
     respond_to do |format|
       format.html {}
@@ -12,43 +12,29 @@ class TweetsCloudController < ApplicationController
 
 	def cloud_tag
 	
-       	 
+    @back_ground_url = 'assets/cloud5.jpg'
+    
     user = params["username"].to_s
 
 
-    #    response =  RestClient.get("https://twitter.com/statuses/user_timeline/"+user+".json"){|response, request, result| response }
-    response =  RestClient.get("http://api.twitter.com/1/statuses/user_timeline.json?id="+user+"&count=200&page=1"){|response, request, result| response }
+    response =  RestClient.get("http://api.twitter.com/1/statuses/user_timeline.json?id="+user+"&count=1000&page=1"){|response, request, result| response }
 
 
-    puts "*"*100
-    puts "retweets_count : #{response.code}"
-    puts "*"*100
     if response.code == 200
       @generated_json = response.body
 
-
+      
       @hashOfResponse = JSON.parse(@generated_json)
 
+      @name = @hashOfResponse[0]['user']['screen_name'] if @hashOfResponse[0]['user']['screen_name']
+      
       @tweets_count = []
       
       @hashOfResponse.each { |res|
         @tweets_count << res['text'].split.size
       }
 
-      #calculating retweets count
-      
-      @re_tweets_count = []
-      @hashOfResponse.each { |re_twt|
-        @re_tweets_count << re_twt['retweet_count']
-      }
 
-
-
-      puts "^"*100
-      puts "retweets_count : #{@re_tweets_count}"
-      puts "^"*100
-
-      
       # iterate over the array, counting duplicate entries
       @actual_count = Hash.new(0)
 
@@ -58,16 +44,21 @@ class TweetsCloudController < ApplicationController
       end
 
       @count = @actual_count.sort_by {|key,value| value }.reverse
-
+      
       respond_to do |format|
         format.html {}
       end
+      
     elsif response.code == 404
-      @response_text  =  " Sorry That Page Doesnot Found"
+      @response_text  =  " Sorry! That Page Does not Found"
       respond_to do |format|
         format.html {redirect_to root_path(:response_text=>@response_text)}
       end
-
+    elsif response.code == 500
+      @response_text  =  " Sorry! Server Error"
+      respond_to do |format|
+        format.html {redirect_to root_path(:response_text=>@response_text)}
+      end
     else
       @response_text  =  " Not Found"
       respond_to do |format|
